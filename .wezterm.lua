356b96d3f915fbef66ef79198c5a0f7017425d01
cipherdev <cadientejomel0423@gmail.com>
2025-08-09 10:51:20 +0800

    Initial commit

---
+++ .wezterm.lua
@@ -0,0 +1,41 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+-- local dimmer = { brightness = 0.1 }
+
+wezterm.font_with_fallback({
+	{ family = "JetBrainsMono NF", style = "Normal", scale = 1 },
+	{ family = "Cambria Math", scale = 1.0 },
+	"FiraCode Nerd Font",
+})
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+-- config.color_scheme = "Guezwhoz"
+config.color_scheme = "rose-pine"
+-- config.font = wezterm.font("JetBrainsMono NF")
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 19
+
+-- window
+config.enable_tab_bar = false
+config.window_decorations = "TITLE | RESIZE"
+config.window_background_opacity = 0.5
+config.kde_window_background_blur = true
+
+config.colors = {
+	background = "black",
+}
+
+-- bg
+-- windows only
+-- config.win32_system_backdrop = "Acrylic"
+--
+-- config.background = {
+-- 	{
+-- 		source = {
+-- 			File = "C:\\Users\\cadie\\termbg\\1.jpg",
+-- 		},
+-- 		hsb = dimmer,
+-- 	},
+-- }
+
+return config
